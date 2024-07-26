57c934de61144679a55fcd4d85cdb618f5d97485
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-07-26 15:20:25 +1000

    Adding logging for incoming search requests

--- api/src/main/java/com/ruchij/web/routes/SearchRoute.java
+++ api/src/main/java/com/ruchij/web/routes/SearchRoute.java
@@ -2,18 +2,25 @@ package com.ruchij.web.routes;
 
 import com.ruchij.web.responses.ErrorResponse;
 import io.javalin.apibuilder.EndpointGroup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 
 import static io.javalin.apibuilder.ApiBuilder.get;
 
 public class SearchRoute implements EndpointGroup {
+    private static final Logger logger = LoggerFactory.getLogger(SearchRoute.class);
+
     @Override
     public void addEndpoints() {
         ClassLoader classLoader = getClass().getClassLoader();
 
         get("/{searchTerm}", context -> {
             String searchTerm = context.pathParam("searchTerm");
+
+            logger.info("Incoming search request for \"%s\"".formatted(searchTerm));
+
             InputStream inputStream = classLoader.getResourceAsStream("data/" + searchTerm + ".json");
             if (inputStream != null) {
                 context.result(inputStream);
